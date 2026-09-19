1ab7af6ec38c3c6c0e3fdaf991997f65e02a9629
Joyanta Kumar <joyroy122004@gmail.com>
2026-09-19 22:09:08 +0600

    Taken user input string

--- src/main.asm
+++ src/main.asm
@@ -1,21 +1,55 @@
 [org 0x7c00]
 
-mov ah, 0x0e
-mov bx, variableName
+  mov si, prompt        
+  call print_string
 
-printString:
-  mov al, [bx]
-  cmp al, 0
-  je end
-  int 0x10
-  inc bx
-  jmp printString
+  mov di, name_buffer     ; Point DI to our input buffer
+  call read_string        ; Read characters from keyboard
 
-variableName:
-  db "But the fool on the hill", 0
+  mov si, newline         ; Print newline
+  call print_string
 
-end:
+  mov si, greeting        ; Print "Hello "
+  call print_string
+
+  mov si, name_buffer     ; Print user's name
+  call print_string
+
   jmp $
+
+print_string:
+  mov ah, 0x0e
+  .loop:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .loop
+  .done:
+    ret 
+
+read_string:
+  .loop:
+    mov ah, 0x0         ; BIOS keystroke function
+    int 0x16
+
+    cmp al, 0x0d
+    je .done
+
+    stosb               ; Store bytes in AL to [DI] and increment DI
+
+    mov ah, 0x0e        ; Echo characters to screen using teletype realtime
+    int 0x10
+
+    jmp .loop
+  .done:
+    mov byte [di], 0
+    ret
+
+prompt:   db "Enter your name: ", 0
+greeting: db "Hello ", 0
+newline:  db 0x0d, 0x0a, 0
+name_buffer: times 32 db 0
 
 times 510-($-$$) db 0
 dw 0xaa55
